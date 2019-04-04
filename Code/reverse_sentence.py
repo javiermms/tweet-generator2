31ac68a89452fb7d46c1a1c4283d822c5c3085d8
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-04-04 13:24:47 -0700

    finished challenged

--- Code/reverse_sentence.py
+++ Code/reverse_sentence.py
@@ -4,16 +4,48 @@ import sys
 arr = sys.argv
 arr.pop(0)
 
+def reverse_word(word):
+    list_of_chars = []
+    count = len(word)
+    while count != 0:
+        count -= 1
+        list_of_chars.append(word[count])
+        backwords = ''.join(list_of_chars)
+    return backwords
+
 def reverse(arr):
     sentence_length = len(arr)
     reverse_arr = []
 
     while sentence_length != 0:
         sentence_length -= 1
-        reverse_arr.append(arr[sentence_length])
+        reverse_arr.append(reverse_word(arr[sentence_length]))
 
     return reverse_arr
+
+
           
 if __name__ == '__main__':
     result = reverse(arr)
     print(' '.join(result))
+
+# cheese = "cheese"
+# print(cheese[1])
+
+#in sequencial order get words in array (get one index)
+# after grabing word change words backwords
+# put the new changed word at the front
+# delete old word
+'''
+The cheese is blue
+step one change the word
+append it to the front 
+delete normal word
+eulb si eseehc ehT 
+'''
+# def reverse_sentence(arr):
+#     print(arr)
+#     for words in arr:
+        
+        
+
