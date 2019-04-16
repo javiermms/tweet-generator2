d8b64503432fa6132f66ded2da1051ab886f4438
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-04-16 12:27:21 -0700

    moved env out

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from stochastic_sampling import probability
-from histogram_list import array
+from Code.stochastic_sampling import probability
+from Code.histogram_list import array
 
 HTML = """<html><head><title>My App</title></head><body><h2>{}</h2></body></html>"""
 app = Flask(__name__)
