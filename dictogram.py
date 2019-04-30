4621184221123f97c331949db4558123e0bcd08c
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-04-30 10:30:26 -0700

    finished markov chain

--- dictogram.py
+++ dictogram.py
@@ -1,5 +1,4 @@
 #!python
-
 from __future__ import division, print_function  # Python 2 and 3 compatibility
 
 
---
+++ markov_chain.py
@@ -0,0 +1,91 @@
+"""
+Psuedo-Code c('.'c)
+
+Mission/Task at Hand: Take a corpus and create a markov chain using a dictionary or list of tuples.
+
+The Dictionary Way:
+
+Create a variable called previous_word
+Create a dictionary and loop through the corpus. 
+If the word is not in the dictionary add it. 
+Use the previous_word word as the key.
+Make a dictionary as the value with a key that is the current word and a value of one. 
+
+If the word is in the dictionary loop through the value (inner dictionary).  
+If the current word is not in this dictionary add it setting the key to current word and the value as one.
+If the current word is in this dictionary increment the number by one.
+
+The Way of The Tuple:
+
+(~~~~Coming Soon~~~~)
+
+Things your keeping track of:
+
+- current word
+- previous_word
+- if the word has already been seen 
+
+"""
+
+# make a get related words function and then call it
+
+from dictogram import Dictogram
+
+with open('sample.txt') as f:
+    text = f.read()
+    words_array = text.split()
+
+def create_markov(array):
+    markov_chain = {}
+    arr_length = len(array) - 1
+    counter = 0
+    
+    while counter < arr_length:
+
+        previous_word =  array[counter]
+
+        if counter + 1 <= arr_length:
+            current_word = array[counter + 1]
+
+        print(previous_word + '!')
+        print(current_word)
+
+        if previous_word not in markov_chain:
+            markov_chain[previous_word] = Dictogram([current_word])  
+        else:
+            markov_chain[previous_word].add_count(current_word)
+        
+        counter += 1
+
+
+
+    # counter = 0
+
+    # arr_length = len(array)
+
+    # while counter < len(array):
+    #     previous_word = array[counter]
+        
+    #     if counter < arr_length - 1 and counter != arr_length:
+    #         current_word = array[counter + 1]
+            
+    #     if previous_word not in markov_chain:
+    #         markov_chain[current_word] = None
+
+    #     if markov_chain != None:
+    #         dic = {}
+    #         markov_chain[previous_word] = dic
+    #         dic[current_word] = 1
+            
+    #     counter += 1
+
+    # for word in array: 
+    #     if word not in markov_chain:
+    #         markov_chain[word] = dict()
+
+    # for word in markov_chain:
+    #     print(markov_chain[word])
+    return markov_chain
+
+print(create_markov(words_array))
+
