ec5938adb764e9f81c875e9052b2c60cdb28b785
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-05-16 22:15:21 -0700

    finished create_markov

--- markov_chain_2.0.py
+++ markov_chain_2.0.py
@@ -4,21 +4,21 @@ from stochastic_sampling import probability
 import string
 from clean_up_text import words_array
 
-def get_words(array):
-    pass
+def get_three_words(array):
+    for index in range(len(array)-2):
+        yield(array[index], array[index + 1], array[index + 2])
 
 def create_markov(array):
     markov_chain = {}
-    
-    three_words = get_words(words_array)
+    three_words = (get_three_words(words_array))
     
     for word_1, word_2, word_3 in three_words:
         tuple = (word_1, word_2)
         if tuple not in markov_chain:
-            pass
+            add_tuple = Dictogram([word_3])
+            markov_chain[tuple] = add_tuple
         else:
-            pass
-
+            markov_chain[tuple].add_count(word_3)
     return markov_chain
 
 
